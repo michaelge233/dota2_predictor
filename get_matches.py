0939c70108f5637e03d5dba5c889475356acf87e
ccpcc <michaelge233@gmail.com>
2020-08-16 20:10:43 +0800

    optimize config files

--- get_matches.py
+++ get_matches.py
@@ -3,9 +3,15 @@ import json
 import numpy as np
 import csv
 
-fo=open("id.json","r")
-id_dic=json.loads(fo.read())
+fo=open("steam_api_key.txt","r")
+steam_api_key=fo.read()
 fo.close()
+
+fo=open("our_ids.txt","r")
+we_list=fo.readlines()
+fo.close()
+for i in range(0,len(we_list)):
+   we_list[i]=int(we_list[i])
 
 fo=open("id_name.json","r")
 id_name_dic=json.loads(fo.read())
@@ -15,25 +21,23 @@ for i in id_name_dic:
    if(int(i)>hero_num):
        hero_num=int(i)
 
-api = dota2api.Initialise(id_dic["apikey"])
+api = dota2api.Initialise(steam_api_key)
 
 match_id_list=[]
 fm=open("matches.json","r")
 already_id_list=json.loads(fm.read())
 fm.close()
 
+search_match_num=input("How many matches per person?\n")
 
-we_list=["cccpccc","prince","wzh","wjl","ljy","wzp"]
-we_id_list=[]
 for we in we_list:
-   we_id_list.append(id_dic[we])
-   hist = api.get_match_history(account_id=id_dic[we],matches_requested=10)
+   hist = api.get_match_history(account_id=we,matches_requested=search_match_num)
 
    for match in hist["matches"]:
       if not (match["match_id"] in match_id_list or match["match_id"] in already_id_list):
          match_id_list.append(match["match_id"])
 
-print("共",len(match_id_list),"场待查")
+print("Totally",len(match_id_list),"matches have not downloaded")
 
 fo=open("X.csv","a",newline='')
 writer=csv.writer(fo)
@@ -45,7 +49,7 @@ for match_id in match_id_list:
     
     is_radiant=True
     for player in match["players"]:
-        if player["account_id"] in we_id_list:
+        if player["account_id"] in we_list:
             if player["player_slot"]<5:
                 is_radiant=True
                 break
@@ -80,11 +84,11 @@ for match_id in match_id_list:
     already_id_list.append(match_id)
     effect_num=effect_num+1
     if effect_num%10==0:
-        print("已添加",effect_num,"场")
+        print(effect_num,"matches already")
 
 fm=open("matches.json","w")
 fm.write(json.dumps(already_id_list))
 fm.close()
 fo.close()
-print("共添加",effect_num,"场")
-input()    
+print("Totally",effect_num,"effective matches were added to the dataset")
+input("Press ENTER to exit")    
