0a08c293e7b2863a961f2e1a912e755123ba2743
ccpcc <michaelge233@gmail.com>
2020-08-17 17:42:18 +0800

    add tips

--- get_id_name.py
+++ get_id_name.py
@@ -1,7 +1,12 @@
 import dota2api
 import json
 
-api = dota2api.Initialise("317D0CD131B4DB6D6A6CE5F1843DEB5A")
+fo=open("steam_api_key.txt","r")
+steam_api_key=fo.read()
+fo.close()
+
+api = dota2api.Initialise(steam_api_key)
+print("Downloading...")
 hero_dict = api.get_heroes()
 heroes = hero_dict['heroes']
 hero_id={}
@@ -11,5 +16,5 @@ for i in heroes:
 fo=open("id_name.json","w")
 fo.write(json.dumps(hero_id))
 fo.close()
-print(hero_id)
-
+print("Finished")
+input("Press ENTER to exit")
